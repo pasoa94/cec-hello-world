b02d3af89e928a1bc106a607272b181d0f0d1ec7
pasoa94 <pasoa@hotmail.com>
2018-02-17 18:05:02 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,10 @@ from datetime import datetime
 application = Flask(__name__)
 
 @application.route("/")
+
 def hello():
     #open the log file
-    with open("/mnt/log", "w") as log: #append mode
+    with open("/mnt/log", "a") as log: #append mode
         log.write(str(datatime.now()) + ":" + socket.gethostname() + "\n") #write a line
     
     #read the log history
