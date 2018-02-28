54e4e8567e3298bba609da9ad73c45e0ea55839b
pasoa94 <pasoa@hotmail.com>
2018-02-28 18:26:02 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,11 +8,11 @@ application = Flask(__name__)
 
 def hello():
     #read the log file and update it with the new log
-    with open("/mnt/log", "w") as f:
+    with open("/mnt/log_history", "a") as f:
         f.write(str(datetime.now()) +"   --->   " + socket.gethostname() + "<br>\n") 
     
     #open the log file for printing it in the body of the application
-    f = open("/mnt/log","r")
+    f = open("/mnt/log_history","r")
     foo = f.read()
     f.close()
     
