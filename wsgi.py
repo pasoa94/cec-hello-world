86372abdef9fc11f6350201167cea8da05d328f2
pasoa94 <pasoa@hotmail.com>
2018-02-28 18:23:43 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,27 +7,17 @@ application = Flask(__name__)
 @application.route("/")
 
 def hello():
-    #open the log file
-    #with open("/mnt/log", "w") as log: #append mode
-    #    log.write(str(datatime.now()) + ":" + socket.gethostname() + "\n<br>") #write a line
+    #read the log file and update it with the new log
+    with open("/mnt/log", "w") as f:
+        f.write(str(datetime.now()) +"   --->   " + socket.gethostname() + "<br>\n") 
     
-    #read the log history
-    #log_file = open("/mnt/log","r")
-    #log_history = log_file.read()
-    #log_file.close()  
-    
-    #txt = "<html><title>CEC</title><body>"+log_history+"</body></html>"
-    
-    with open("/mnt/logfile", "w") as f:
-        f.write(str(datetime.now()) +"---> " + socket.gethostname() + "<br>\n") 
-    
-    #open logfile and return its contents
-    f = open("/mnt/logfile","r")
+    #open the log file for printing it in the body of the application
+    f = open("/mnt/log","r")
     foo = f.read()
     f.close()
-    txt = "<html><title>Song3</title><body>" + foo + "</body></html>"
     
-    return txt
+    return "<html><title>Song3</title><body>" + foo + "</body></html>"
+    
 
 if __name__ == "__main__":
     application.run()
